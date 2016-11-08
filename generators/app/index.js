2a8d25f95991232615172011d490dd704490eeda
Joscelyn Jean <joscelyn.jean@gmail.com>
2016-11-08 13:22:40 -0500

    Templating

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,29 +4,60 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = yeoman.Base.extend({
-  prompting: function () {
-    // Have Yeoman greet the user.
+
+  welcome: function() {
     this.log(yosay(
       'Welcome to the doozie ' + chalk.red('generator-angular2-spring-boot-mvc') + ' generator!'
     ));
+  },
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    return this.prompt(prompts).then(function (props) {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    }.bind(this));
+  prompting: function() {
+    return this.prompt([
+      // Artifact ID in the pom.xml
+      {
+        type    : 'input',
+        name    : 'artifactId',
+        message : 'Your artifact ID'
+      },
+      // Package name for the pom.xml, folder generation and java classes
+      {
+        type    : 'input',
+        name    : 'package',
+        message : 'Your package (ex: com.organization)'
+      }
+    ]).then(function (props) {
+        // Properties to use later
+        this.props = props;
+      }.bind(this));
   },
 
   writing: function () {
-    this.fs.copy(
-      this.templatePath('.'),
-      this.destinationPath('.')
+    var folderHierarchy = this.props.package.replace(/\./g, "//");
+
+    // Copy java sources
+    this.fs.copyTpl(
+      this.templatePath('src/main/java'),
+      this.destinationPath('src/main/java/' + folderHierarchy),
+      {
+        artifactId: this.props.artifactId,
+        package: this.props.package
+      }
+    );
+
+    // Copy pom file
+    this.fs.copyTpl(
+      this.templatePath('pom.xml'),
+      this.destinationPath('pom.xml'),
+      {
+        artifactId: this.props.artifactId,
+        package: this.props.package
+      }
+    );
+
+    // Copy webapp
+    this.fs.copyTpl(
+      this.templatePath('src/main/webapp'),
+      this.destinationPath('src/main/webapp')
     );
   },
 
--- generators/app/templates/src/main/java/controller/HelloController.java
+++ generators/app/templates/src/main/java/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.dummy.controller;
+package <%= package %>.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
